982d1e73b18d8f93af7e86416266254f67e0ecc4
benji-devw <bennav.devweb@gmail.com>
2023-05-23 00:35:38 +0200

    rename file + add location.href to factory + get media

--- scripts/factories/photographerFactory.js
+++ scripts/factories/photographerFactory.js
@@ -1,19 +1,23 @@
+// import {Photographer} from "../pages/photographer";
+
 function photographerFactory(data) {
   console.log(data);
-  const { name, city, tagline, price, portrait } = data;
+  const { id, name, city, tagline, price, portrait } = data;
 
   const picture = `assets/photographers/${portrait}`;
+
 
   function getUserCardDOM() {
     const article = document.createElement("article");
     const articleHead = document.createElement("div");
     const articleBody = document.createElement("div");
+    const linkCard = document.createElement("a");
     const img = document.createElement("img");
     const titleCard = document.createElement("h2");
     const cityCard = document.createElement("span")
     const taglineCard = document.createElement("p")
     const priceCard = document.createElement("p")
-    
+
     articleHead.classList.add("article_head");
     articleBody.classList.add("article_body");
     cityCard.classList.add("article_body_city");
@@ -21,14 +25,26 @@ function photographerFactory(data) {
     priceCard.classList.add("article_body_price");
     
     img.setAttribute("src", picture);
+    img.setAttribute("alt", name);
+    linkCard.setAttribute("src", "/photographer.html")
+    
+    img.addEventListener("click", function() {
+      // Photographer()
+      window.location.href = `/photographer.html` + `?id=${id}`;
+    });
+
+    // linkCard.textContent = "/photographer.html"
     titleCard.textContent = name;
     cityCard.textContent = city;
     taglineCard.textContent = tagline;
     priceCard.textContent = price + "€/jour";
 
-    article.appendChild(articleHead)
-    article.appendChild(articleBody)
-    articleHead.appendChild(img)
+    article.appendChild(articleHead);
+    article.appendChild(articleBody);
+    // articleHead.appendChild(linkCard);
+    // linkCard.appendChild(img)
+    
+    articleHead.appendChild(img);
     articleBody.appendChild(titleCard);
     articleBody.appendChild(cityCard);
     articleBody.appendChild(taglineCard);
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,17 @@
 //Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographerData() {
+  await fetch('../../data/photographers.json')
+  .then(response => response.json())
+  .then(data => {
+    const filterById = data.media.filter(id => id.photographerId === 82)
+    console.log(filterById);
+  })
+  .catch(err => console.log(err))
+}
+
+function Photographer() {
+  getPhotographerData()
+  // return console.log('test');
+}
+
+Photographer()
