68f7edf313be70c2d15f9c30a6a6607c947f7b85
benji-devw <bennav.devweb@gmail.com>
2023-05-25 23:44:37 +0200

    [photographer.js] getIdQuery + header infos

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -17,13 +17,52 @@ function getIdQuery() {
   return urlParams.get('id');
 }
 
+async function Profile() {
+  const { photographers } = await getPhotographers();
+  const getPhotographer = photographers.filter(user => user.id == getIdQuery())
+  
+  const nameDiv = document.querySelector('.name')
+  const cityDiv = document.querySelector('.city')
+  const taglineDiv = document.querySelector('.tagline')
+  const imageDiv = document.querySelector('.image')
+  const img = document.createElement("img");
+  imageDiv.appendChild(img)
+  
+  nameDiv.textContent = getPhotographer[0].name
+  cityDiv.textContent = getPhotographer[0].city
+  taglineDiv.textContent = getPhotographer[0].tagline
+  
+  img.setAttribute("src", `assets/photographers/${getPhotographer[0].portrait}`)
+  img.setAttribute("alt", getPhotographer[0].name)
+
+}
+
+async function Medias() {
+  const { media } = await getPhotographers();
+  const getMedia = media.filter(id => id.photographerId == getIdQuery());
+  console.log(getMedia);
+  
+  const gallery = document.querySelector('.gallery')
+
+  const imageElements = getMedia.map(imageUrl => {
+    const imgElement = document.createElement('img');
+    imgElement.src = `assets/images/${imag}/${imageUrl.image}`;
+    return imgElement;
+  });
+  
+  imageElements.forEach(imageElement => {
+    gallery.appendChild(imageElement);
+  });
+
+  return gallery
+
+}
 
 async function photographer() {
-  const { photographers, media } = await getPhotographers();
-  const getPhotographer = photographers.filter(user => user.id == getIdQuery())
-  const getMedia = media.filter(id => id.photographerId == getIdQuery());
 
-  console.log(getPhotographer);
+  Profile()
+  Medias()
+  
 }
 
 photographer();
