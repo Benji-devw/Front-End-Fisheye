50232265937197209e3a62887136fe4c1a47b876
benji-devw <ben.nav@pm.me>
2023-06-07 22:36:34 +0200

    prepare for slider

--- scripts/pages/photographerPage.js
+++ scripts/pages/photographerPage.js
@@ -1,6 +1,7 @@
 import PhotographersApi from "../../data/data.js";
 import MediasFactory from "../factories/MediasFactory.js";
 import ContactModal from "../utils/contactForm.js";
+import Slider from "../utils/slider.js";
 
 function getIdQuery() {
   const queryString = window.location.search;
@@ -18,29 +19,37 @@ export default class PhotographerPage {
     this.$photographer_infos = document.querySelector('#photographer_infos')
     this.$image_header = document.querySelector('#image_header')
     this.$gallery = document.querySelector('.gallery');
-    this.$openContactModal = document.querySelector(".contact_button");
-    this.$closeContactModal = document.querySelector(".close_modal")
-    this.$submitContactButton = document.querySelector(".sumit_button");
-
-    this.$photographersApi = new PhotographersApi("../../data/photographers.json")
+    this.$openContactModal = document.querySelector('.contact_button');
+    this.$closeContactModal = document.querySelector('.close_modal')
+    this.$submitContactButton = document.querySelector('.sumit_button');
+    
+    this.$photographersApi = new PhotographersApi('../../data/photographers.json')
   }
-
+  
   async main() {
     const photographer = await this.$photographersApi.getPhotographer(getIdQuery())
-    // console.log(photographer);
-
-
+    
+    
     const {$_banner, $_image} = photographer.createPhotographerBanner()
     this.$photographer_infos.innerHTML = $_banner
     this.$image_header.innerHTML = $_image
-
-
+    
+    
     photographer.media.forEach(media => {
       const factory = new MediasFactory(photographer.name, media);
       const medias = factory.createMedia()
       // console.log(medias);
       this.$gallery.innerHTML += medias
     });
+    
+    
+    const slider = new Slider(photographer.media)
+    const $card = document.querySelector('.card')
+    $card.addEventListener("click", () => {
+      // slider.test()
+      console.log('test');
+
+    })
 
 
     const contactModal = new ContactModal(photographer.name);
