7315290dd31e95dd4f0ca8510ee58428050b2875
Torkh <ben.nav@pm.me>
2023-08-20 16:17:50 +0200

    comment/add comments to slider func

--- scripts/utils/slider.js
+++ scripts/utils/slider.js
@@ -3,7 +3,7 @@
 **********************************/
 export default class SliderModel {
   constructor(data) {
-    this.name =  data.name
+    this.name = data.name
     this.medias = data.medias
   }
 
@@ -22,21 +22,27 @@ export default class SliderModel {
 
     buttons.forEach(btn => {
       btn.addEventListener('click', () => {
+        // If click next or prev btn => return 1 or -1
         const offset = btn.dataset.carouselBtn === 'next' ? 1 : -1
+        // return ul element
         const slides = btn.closest('.slider-body').querySelector('[data-slides]')
+        // return active li element
         const activeSlide = slides.querySelector('[data-active]')
-
+        // Create copy child element in array for use indexOf func
+        // Search index of active element and add it offset result 1 or -1
         let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        // Condition for boucle in slider from first and last element
         if (newIndex < 0) newIndex = slides.children.length - 1
         if (newIndex >= slides.children.length) newIndex = 0
 
+        // Display or hide element with data-active attr
         slides.children[newIndex].dataset.active = "true"
         delete activeSlide.dataset.active
       })
     })
   }
 
-  
+
   createSlider(currentId) {
     return `
       <div class="slider-container" aria-label="Slider Medias">
@@ -48,14 +54,14 @@ export default class SliderModel {
     
           <div class="slider-body">
             <button class="carousel-nav prev" data-carousel-btn="prev" aria-label="Previous image">&lt;</button>
-          
+            
             <div id="image-carousel" class="carousel" data-carousel>
               <ul data-slides>
-                ${this.medias.map((media) => media.createSliderItem(currentId))}
+                 ${this.medias.map((media) => media.createSliderItem(currentId))}
               </ul>
             </div>
-            <button class="carousel-nav next" data-carousel-btn="next" aria-label="Next image">&gt;</button>
             
+            <button class="carousel-nav next" data-carousel-btn="next" aria-label="Next image">&gt;</button>
           </div>
         </div>
       </div>
